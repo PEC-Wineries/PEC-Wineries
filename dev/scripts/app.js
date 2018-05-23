9bb6ecc8c2454e83a132dd89c20300035046f47a
S M <38589300+CoolCoolSweet@users.noreply.github.com>
2018-05-23 17:09:18 -0400

    Initial commit.

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,14 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 class App extends React.Component {
-  render() {
-    return (
-      <div>
-        Hello
-      </div>
-    )
-  }
+	constructor() {
+		super();
+		this.state = {
+			wines: [],
+			search: ""
+		}
+
+		this.handleInput = this.handleInput.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	componentDidMount() {
+	
+	}
+	handleInput (e) {
+		e.preventDefault();
+		this.setState({
+			search: e.target.value
+		})
+	}
+	handleSubmit (e) {
+		e.preventDefault();
+		axios({
+			url: 'https://lcboapi.com/products',
+			params: {
+				access_key: "MDoxNDEyMWE4Ni01ZGZiLTExZTgtYTVjYi1jN2JlMmFhMTZiNmQ6SzlralhKWGRwNWVXclp0R1VhcEJFNUU3WWRaTFVLTWkxRW5l",
+				q: this.state.search
+			}
+		}).then((res) => {
+			this.setState({
+				wines: res.data.result
+			});
+			console.log(this.state.wines)
+		});
+	}
+	render() {
+		return (
+			<div>
+				<form action="" onSubmit={this.handleSubmit} >
+				<input onChange={this.handleInput} value={this.search} type="text"/>
+				</form>
+				<ul>
+				{this.state.wines.map((wine) => {
+					return <li key={wine.id}>{wine.name}</li>
+				})}
+				</ul>
+			</div>
+		)
+	}
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
