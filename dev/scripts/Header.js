ee5e89a5744fac7f23c308b047b4faf33eb65951
S M <38589300+CoolCoolSweet@users.noreply.github.com>
2018-05-24 12:55:09 -0400

    Header componenet made. CSS normalize partial made. Both imported

---
+++ dev/scripts/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Header extends React.Component {
+	render() {
+		return (
+			<header>
+				<h1>Prince</h1>
+				<h1>Edward</h1>
+				<h1>Country</h1>
+				<h1>Wineries</h1>
+			</header>
+		)
+	}
+}
+
+export default Header;
